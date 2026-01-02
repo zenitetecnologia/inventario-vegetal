6bbf40eb5597efe48bbe74aecba25a25b8ecb7b8
YuriLucas7 <lucasy973@gmail.com>
2026-01-02 17:25:45 -0300

    add models

---
+++ Zenite.IventarioVegetal.Server/Models/ItemEstoque.cs
@@ -0,0 +1,11 @@
+﻿namespace Zenite.IventarioVegetal.Server.Models
+{
+    public class ItemEstoque
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; } 
+        public decimal Quantidade { get; set; }
+        public DateTime Data { get; set; }
+        public bool Geladeira { get; set; }
+    }
+}
