389f4ea6beb26afcebe34c9a0f494ffd14f9370c
Joaofernaandess <jf030626@gmail.com>
2026-01-05 17:27:43 -0300

    commit 05-01

---
+++ Zenite.IventarioVegetal.Server/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zenite.IventarioVegetal.Server.Entities
+{
+   
+        public class Produto
+        {
+            public int Id { get; set; } 
+            public string Nome { get; set; } = string.Empty;
+            public decimal Preco { get; set; } 
+        }
+    
+}
